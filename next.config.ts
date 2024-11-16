369a161dc1a4e246e3ffc6d79f382b918c46588d
Alex Avila <alex.avila.09@outlook.com>
2024-11-16 03:02:31 -0700

    feat: redirect /decks to /
    re #2

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/decks",
+        destination: "/",
+        // TODO: reconsider if permanent should be false or true
+        permanent: false,
+      },
+    ]
+  },
 }
 
 export default nextConfig
