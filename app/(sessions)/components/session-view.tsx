bed36dd758ecf7aa89a75295194c5a7d623eb21c
Alex Avila <alex.avila.09@outlook.com>
2024-12-09 04:28:09 -0700

    feat: refactor for better maintainability and code quality, add improved finished state
    handling the UI states using MODES worked but it was too limiting to only use one string that
    alternated between modes. That works if the UI depends on one factor but the slightly increased
    complexity of the UI depended on multiple factors/variables, what action the user could take at the
    current moment (ANSWER, START_NEXT, FINISH), and what was the result of the user's last action
    (correct or incorrect answer). technically more but those are the main ones. Refactored to use a
    reducer to always have more data about the ui state and for the added abstraction. Using an object
    with useState could've been viable too. Also, added a "Finish!" button label and made it so that
    that button in that state returns the user to the dashboard

--- app/(sessions)/components/session-view.tsx
+++ app/(sessions)/components/session-view.tsx
@@ -1,34 +1,62 @@
 "use client"
 
-import { useState, useMemo, FormEvent } from "react"
+import { useRouter } from "next/navigation"
+import { useState, useMemo, useReducer, useCallback } from "react"
+import { clsx } from "clsx"
+import { Card } from "@/app/db/schema"
 import { Flashcard } from "@/app/components/ui/flashcard"
 import { Progress } from "@/app/components/ui/progress"
-import { Card } from "@/app/db/schema"
 import { Input } from "@/app/components/ui/input"
 import { Button } from "@/app/components/ui/button"
 
-interface SessionViewProps {
-  deckId: string
-  cards: Card[]
+interface SessionStateState {
+  phase: "interrogation" | "answered" | "finished"
+  wasCorrect?: boolean
 }
 
-enum MODES {
-  interrogation = "interrogation",
-  answeredCorrect = "answeredCorrect",
-  answeredIncorrect = "answeredIncorrect",
-  finished = "finished",
-}
+const initialSessionState = { phase: "interrogation" } as SessionStateState
 
-type ModesStrings = keyof typeof MODES
+type SessionStateAction =
+  | { type: "START_NEXT" }
+  | { type: "ANSWER"; wasCorrect: boolean }
+  | { type: "FINISH"; wasCorrect: boolean }
+
+// reducer manages data that we use to determine the UI states in a centralized place
+// and provides minimal abstraction via actions types to simplify the logic in the component
+function sessionStateReducer(
+  state: SessionStateState,
+  action: SessionStateAction
+): SessionStateState {
+  const { type } = action
+
+  switch (type) {
+    case "START_NEXT": {
+      return { phase: "interrogation" }
+    }
+    case "ANSWER": {
+      return { phase: "answered", wasCorrect: action.wasCorrect }
+    }
+    case "FINISH": {
+      return { phase: "finished", wasCorrect: action.wasCorrect }
+    }
+    default: {
+      throw new Error(`Unknown action type ${type}`)
+    }
+  }
+}
 
 type CardAndIndex = [Card, number]
+let initialCard: CardAndIndex | null = null
 function* cardsGenerator(cards: Card[]): Generator<CardAndIndex, void> {
   for (let i = 0; i < cards.length; i++) {
     yield [cards[i], i]
   }
 }
 
-let initialCard: CardAndIndex | null = null
+interface SessionViewProps {
+  deckId: string
+  cards: Card[]
+}
 
 // TODO: maybe rethink the leveling system. to make sure we only ever have 5 levels,
 // ...it could take more consecutive correct answers to reach a new level
@@ -39,73 +67,100 @@ export function SessionView({ deckId, cards }: SessionViewProps) {
     throw new Error("no cards to review")
   }
 
+  const router = useRouter()
+
   // NOTE: unnecessary use of a generator function, but it works and I wanted to practice it
   const getNextCard = useMemo(() => {
+    // NOTE: this should never rerun b/c 'cards' shouldn't change,
+    // ...but if they changed, currentCard should be set to initialCard again
     const gen = cardsGenerator(cards)
     initialCard = gen.next().value!
 
     return () => gen.next().value
   }, [cards])
 
+  const [sessionState, dispatchSessionState] = useReducer(
+    sessionStateReducer,
+    initialSessionState
+  )
   const [currentCard, setCurrentCard] = useState(initialCard!)
   const [answer, setAnswer] = useState("")
-  const [mode, setMode] = useState<ModesStrings>("interrogation")
 
-  const checkAnswer = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+  const notInterrogating = sessionState.phase !== "interrogation"
+  const isFinished = sessionState.phase === "finished"
 
-    // TODO: add feedback on input or whatever for either for these two options
-    if (currentCard[0].back.toLowerCase() === answer.trim().toLowerCase()) {
-      setMode(MODES.answeredCorrect)
-    } else {
-      // TODO: disable the input (b/c they can't change their answer at this point) and move focus to next button i guess
-      setMode(MODES.answeredIncorrect)
-    }
-  }
+  const progress =
+    ((currentCard[1] + (!notInterrogating ? 0 : 1)) / cards.length) * 100
 
-  const resetForNextReview = (nextCard: CardAndIndex) => {
+  let buttonLabel = "Submit"
+  if (isFinished) buttonLabel = "Finish!"
+  if (notInterrogating) buttonLabel = "Next"
+
+  const checkAnswer = useCallback(() => {
+    // set to finish if we're checking the answer of the final card
+    const isLast = Boolean(currentCard[1] >= cards.length - 1)
+    dispatchSessionState({
+      type: isLast ? "FINISH" : "ANSWER",
+      wasCorrect:
+        currentCard[0].back.toLowerCase() === answer.trim().toLowerCase(),
+    })
+  }, [currentCard, answer, cards])
+
+  const resetForNextReview = useCallback((nextCard: CardAndIndex) => {
     setAnswer("")
-    setMode(MODES.interrogation)
+    dispatchSessionState({ type: "START_NEXT" })
     setCurrentCard(nextCard)
-  }
+  }, [])
 
-  const nextReview = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+  const nextReview = useCallback(() => {
     const nextCard = getNextCard()
     if (nextCard) {
       resetForNextReview(nextCard)
     }
-  }
+  }, [getNextCard, resetForNextReview])
 
-  const progress =
-    ((currentCard[1] + (mode === MODES.interrogation ? 0 : 1)) / cards.length) *
-    100
+  const handleSubmit = useCallback(
+    (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault()
 
-  // use 'as string[]' to make typescript happy
-  const flipped = (
-    [MODES.answeredCorrect, MODES.answeredIncorrect] as string[]
-  ).includes(mode)
+      if (isFinished) {
+        router.push("/")
+        return
+      }
+
+      if (!notInterrogating) {
+        checkAnswer()
+      } else {
+        nextReview()
+      }
+    },
+    [router, checkAnswer, nextReview, isFinished, notInterrogating]
+  )
 
   return (
     <>
       <Progress value={progress} />
-      <div className="mt-6 space-y-4">
-        <Flashcard card={currentCard[0]} canFlip={false} flipped={flipped} />
-        <form
-          className="flex gap-2"
-          onSubmit={event =>
-            mode === MODES.interrogation
-              ? checkAnswer(event)
-              : nextReview(event)
-          }
-        >
+      <div className="mt-6 space-y-6">
+        <Flashcard
+          card={currentCard[0]}
+          canFlip={false}
+          flipped={notInterrogating}
+        />
+        <form className="flex gap-2" onSubmit={handleSubmit}>
           <Input
+            className={`text-center ${clsx({
+              "bg-green-500/20 focus-visible:ring-green-500":
+                notInterrogating && sessionState.wasCorrect,
+              "bg-red-500/20 focus-visible:ring-red-500":
+                notInterrogating && !sessionState.wasCorrect,
+            })}`}
             required
             value={answer}
+            readOnly={notInterrogating}
             onChange={event => setAnswer(event.target.value)}
           />
-          <Button type="submit" variant="outline">
-            {mode === MODES.interrogation ? "Submit" : "Next"}
+          <Button className="min-w-[6em]" type="submit">
+            {buttonLabel}
           </Button>
         </form>
       </div>
