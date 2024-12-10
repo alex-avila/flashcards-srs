69f4167151957a6f8c469bbdd8ef766b9e247c39
Alex Avila <alex.avila.09@outlook.com>
2024-12-09 23:07:52 -0700

    feat: improve session view component logic, mainly how it's organized

--- app/(sessions)/components/session-view.tsx
+++ app/(sessions)/components/session-view.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useRouter } from "next/navigation"
-import { useState, useMemo, useReducer, useCallback } from "react"
+import { useState, useMemo, useReducer } from "react"
 import { clsx } from "clsx"
 import { Card } from "@/app/db/schema"
 import { Flashcard } from "@/app/components/ui/flashcard"
@@ -13,8 +13,6 @@ interface SessionStateState {
   phase: "interrogation" | "answered" | "finished"
   wasCorrect?: boolean
 }
-
-const initialSessionState = { phase: "interrogation" } as SessionStateState
 
 type SessionStateAction =
   | { type: "START_NEXT" }
@@ -45,12 +43,35 @@ function sessionStateReducer(
   }
 }
 
+function useSessionState(initialState: SessionStateState) {
+  const [state, dispatch] = useReducer(sessionStateReducer, initialState)
+
+  // helper variables
+  const isAnswered = state.phase !== "interrogation"
+  const isFinished = state.phase === "finished"
+
+  return { state, dispatch, isAnswered, isFinished }
+}
+
 type CardAndIndex = [Card, number]
-let initialCard: CardAndIndex | null = null
 function* cardsGenerator(cards: Card[]): Generator<CardAndIndex, void> {
   for (let i = 0; i < cards.length; i++) {
     yield [cards[i], i]
   }
+}
+
+function useCardSequence(cards: Card[]) {
+  // NOTE: unnecessary use of a generator function, but it works and I wanted to practice it
+  const getNextCard = useMemo(() => {
+    // NOTE: this should never rerun b/c 'cards' shouldn't change,
+    // ...but if they changed, currentCard should be set to initialCard again
+    const gen = cardsGenerator(cards)
+    return () => gen.next().value
+  }, [cards])
+
+  const initialCard = useMemo(() => getNextCard()!, [getNextCard])
+
+  return { getNextCard, initialCard }
 }
 
 interface SessionViewProps {
@@ -58,10 +79,7 @@ interface SessionViewProps {
   cards: Card[]
 }
 
-// TODO: maybe rethink the leveling system. to make sure we only ever have 5 levels,
-// ...it could take more consecutive correct answers to reach a new level
-// ...(e.g. 3 correct answers after level 2 to reach level 3)
-export function SessionView({ deckId, cards }: SessionViewProps) {
+export function SessionView({ cards }: SessionViewProps) {
   if (!cards.length) {
     // TODO: actually handle this case
     throw new Error("no cards to review")
@@ -69,98 +87,80 @@ export function SessionView({ deckId, cards }: SessionViewProps) {
 
   const router = useRouter()
 
-  // NOTE: unnecessary use of a generator function, but it works and I wanted to practice it
-  const getNextCard = useMemo(() => {
-    // NOTE: this should never rerun b/c 'cards' shouldn't change,
-    // ...but if they changed, currentCard should be set to initialCard again
-    const gen = cardsGenerator(cards)
-    initialCard = gen.next().value!
+  const {
+    state: sessionState,
+    dispatch: dispatchSessionState,
+    isAnswered,
+    isFinished,
+  } = useSessionState({ phase: "interrogation" } as SessionStateState)
 
-    return () => gen.next().value
-  }, [cards])
+  const { getNextCard, initialCard } = useCardSequence(cards)
 
-  const [sessionState, dispatchSessionState] = useReducer(
-    sessionStateReducer,
-    initialSessionState
-  )
   const [currentCard, setCurrentCard] = useState(initialCard!)
   const [answer, setAnswer] = useState("")
 
-  const notInterrogating = sessionState.phase !== "interrogation"
-  const isFinished = sessionState.phase === "finished"
-
-  const progress =
-    ((currentCard[1] + (!notInterrogating ? 0 : 1)) / cards.length) * 100
-
-  let buttonLabel = "Submit"
-  if (isFinished) buttonLabel = "Finish!"
-  else if (notInterrogating) buttonLabel = "Next"
-
-  const checkAnswer = useCallback(() => {
-    // set to finish if we're checking the answer of the final card
-    const isLast = Boolean(currentCard[1] >= cards.length - 1)
-    dispatchSessionState({
-      type: isLast ? "FINISH" : "ANSWER",
-      wasCorrect:
-        currentCard[0].back.toLowerCase() === answer.trim().toLowerCase(),
-    })
-  }, [currentCard, answer, cards])
-
-  const resetForNextReview = useCallback((nextCard: CardAndIndex) => {
+  const resetForNextReview = (nextCard: CardAndIndex) => {
     setAnswer("")
     dispatchSessionState({ type: "START_NEXT" })
     setCurrentCard(nextCard)
-  }, [])
+  }
 
-  const nextReview = useCallback(() => {
-    const nextCard = getNextCard()
-    if (nextCard) {
-      resetForNextReview(nextCard)
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    if (isFinished) {
+      router.push("/")
+      return
     }
-  }, [getNextCard, resetForNextReview])
 
-  const handleSubmit = useCallback(
-    (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault()
+    if (!isAnswered) {
+      const isLast = Boolean(currentCard[1] >= cards.length - 1)
+      const wasCorrect =
+        currentCard[0].back.toLowerCase() === answer.trim().toLowerCase()
 
-      if (isFinished) {
-        router.push("/")
-        return
+      dispatchSessionState({
+        type: isLast ? "FINISH" : "ANSWER",
+        wasCorrect,
+      })
+    } else {
+      const nextCard = getNextCard()
+      if (nextCard) {
+        resetForNextReview(nextCard)
       }
+    }
+  }
 
-      if (!notInterrogating) {
-        checkAnswer()
-      } else {
-        nextReview()
-      }
-    },
-    [router, checkAnswer, nextReview, isFinished, notInterrogating]
-  )
+  const progress =
+    ((currentCard[1] + (!isAnswered ? 0 : 1)) / cards.length) * 100
 
   return (
     <>
       <Progress value={progress} />
       <div className="mt-6 space-y-6">
-        <Flashcard
-          card={currentCard[0]}
-          canFlip={false}
-          flipped={notInterrogating}
-        />
+        <Flashcard card={currentCard[0]} canFlip={false} flipped={isAnswered} />
         <form className="flex gap-2" onSubmit={handleSubmit}>
+          <label htmlFor="answer-input" className="sr-only">
+            Answer
+          </label>
           <Input
+            id="answer-input"
             className={`text-center ${clsx({
               "bg-green-500/20 focus-visible:ring-green-500":
-                notInterrogating && sessionState.wasCorrect,
+                isAnswered && sessionState.wasCorrect,
               "bg-red-500/20 focus-visible:ring-red-500":
-                notInterrogating && !sessionState.wasCorrect,
+                isAnswered && !sessionState.wasCorrect,
             })}`}
             required
+            autoCorrect="off"
+            autoComplete="off"
+            autoCapitalize="none"
+            spellCheck="false"
             value={answer}
-            readOnly={notInterrogating}
+            readOnly={isAnswered}
             onChange={event => setAnswer(event.target.value)}
           />
           <Button className="min-w-[6em]" type="submit">
-            {buttonLabel}
+            {isFinished ? "Finish!" : isAnswered ? "Next" : "Submit"}
           </Button>
         </form>
       </div>
