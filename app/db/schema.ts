d7b290d313701f6979d7dbe9c408ed693e86b163
Alex Avila <alex.avila.09@outlook.com>
2024-12-12 04:03:54 -0700

    feat: improve database stuff, add seed script, simplify main db index file, implement schemas (v1)

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,7 +1,74 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core"
+import { sql, relations } from "drizzle-orm"
+import {
+  integer,
+  pgTable,
+  varchar,
+  check,
+  timestamp,
+} from "drizzle-orm/pg-core"
 
-export const usersTable = pgTable("users", {
+export const users = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   username: varchar({ length: 255 }).notNull().unique(),
   email: varchar({ length: 255 }).notNull().unique(),
 })
+
+export const usersRelations = relations(users, ({ many }) => ({
+  decks: many(decks),
+}))
+
+export const decks = pgTable(
+  "decks",
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    userId: integer("user_id").references(() => users.id),
+    name: varchar({ length: 60 }).notNull(),
+    description: varchar(),
+    lessonsPerDay: integer("lessons_per_day").default(15),
+    lessonsBatchSize: integer("lessons_batch_size").default(5),
+  },
+  table => [
+    {
+      validLessonsPerDay: check(
+        "valid_lessons_per_day",
+        sql`${table.lessonsPerDay} >= 0 AND ${table.lessonsPerDay} < 101`
+      ),
+      validLessonsBatchSize: check(
+        "valid_lessons_batch_size",
+        sql`${table.lessonsBatchSize} >= 3 AND ${table.lessonsBatchSize} < 11`
+      ),
+    },
+  ]
+)
+
+export const decksRelations = relations(decks, ({ one, many }) => ({
+  user: one(users, { fields: [decks.userId], references: [users.id] }),
+  cards: many(cards),
+}))
+
+export const cards = pgTable(
+  "cards",
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    deckId: integer("deck_id").references(() => decks.id),
+    front: varchar({ length: 55 }).notNull(),
+    back: varchar({ length: 55 }).notNull(),
+    notes: varchar().notNull(),
+    level: integer().default(0),
+    learnedDate: timestamp("learned_date", { mode: "date" }),
+    // TODO: maybe update nextReviewDate with $onUpdate so that the database takes care of the srs kinda implicitly
+    // - would also need a lastAnswerState ('correct', 'incorrect', null) to know how to update the nextReviewDate
+    nextReviewDate: timestamp("next_review_date", { mode: "date" }),
+    lastCorrectDate: timestamp("last_correct_date", { mode: "date" }),
+    // maybe add more like 'lastIncorrectAnswerDate', 'consecutiveCorrectCount', 'internalLevel', etc.
+  },
+  table => [
+    {
+      validLevel: check("valid_level", sql`${table.level} > 0`),
+    },
+  ]
+)
+
+export const cardsRelations = relations(cards, ({ one }) => ({
+  deck: one(decks, { fields: [cards.deckId], references: [decks.id] }),
+}))
---
+++ app/db/seed.ts
@@ -0,0 +1,69 @@
+import "dotenv/config"
+import { drizzle } from "drizzle-orm/node-postgres"
+import * as schema from "./schema"
+
+export const db = drizzle<typeof schema>(process.env.DATABASE_URL!)
+
+async function main() {
+  await seed()
+}
+
+async function seed() {
+  const user: typeof schema.users.$inferInsert = {
+    username: "rickdeckard",
+    email: "rickdeckard@example.com",
+  }
+
+  const [newUser] = await db.insert(schema.users).values(user).returning()
+  console.log("New user created!")
+
+  const deck: typeof schema.decks.$inferInsert = {
+    userId: newUser.id,
+    name: "Japanese Vocabulary",
+    description: "Some japanese vocabulary and hiragana and katakana",
+  }
+  const [newDeck] = await db.insert(schema.decks).values(deck).returning()
+  console.log("New deck created!")
+
+  const cards: Array<typeof schema.cards.$inferInsert> = [
+    {
+      deckId: newDeck.id,
+      front: "犬 (いぬ)",
+      back: "Dog",
+      notes: "Common pet",
+      level: 1,
+    },
+    {
+      deckId: newDeck.id,
+      front: "猫 (ねこ)",
+      back: "Cat",
+      notes: "Popular animal",
+      level: 1,
+    },
+    {
+      deckId: newDeck.id,
+      front: "水 (みず)",
+      back: "Water",
+      notes: "Drinkable",
+      level: 1,
+    },
+    {
+      deckId: newDeck.id,
+      front: "空 (そら)",
+      back: "Sky",
+      notes: "Blue on a clear day",
+      level: 1,
+    },
+    {
+      deckId: newDeck.id,
+      front: "ありがとう",
+      back: "Thank you",
+      notes: "Polite expression",
+      level: 1,
+    },
+  ]
+  await db.insert(schema.cards).values(cards).returning()
+  console.log("new cards created")
+}
+
+main()
