fc079d561ca3659a95120bb25c8ad7ca26ef4990
Alex Avila <alex.avila.09@outlook.com>
2024-12-24 03:41:29 -0700

    chore: remove unnecessary comment

--- app/(sessions)/lessons/[deckPathname]/page.tsx
+++ app/(sessions)/lessons/[deckPathname]/page.tsx
@@ -6,15 +6,6 @@ interface LessonsPageProps {
 }
 
 export default async function LessonsPage({ params }: LessonsPageProps) {
-  // on the server
-  // fetch the cards for the deck in the 'deckPathname' param
-  // filter to only get the ones that are due
-  // (should actually only query for the ones that are due)
-  // construct some sort of current reviews object to use in the javascript
-  // so that the user can actually complete the review with whatever was available at the time
-  // (e.g. if new cards become available for review while doing the reviews, don't consider them
-  // ...when determining if the current session is done)
-
   const pathname = (await params).deckPathname
   const { deck, lessons } = await fetchLessons({ pathname })
 
