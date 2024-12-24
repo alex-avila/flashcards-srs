ed6daa07ff437a81bbbba4108e3ad4c2c8890a8c
Alex Avila <alex.avila.09@outlook.com>
2024-12-23 23:26:15 -0700

    add subsets to avoid next/font error when building on vercel

--- app/fonts.ts
+++ app/fonts.ts
@@ -3,4 +3,5 @@ import { Zen_Kaku_Gothic_New } from "next/font/google"
 export const zenKakuGothicNew = Zen_Kaku_Gothic_New({
   weight: ["400", "500", "700"],
   variable: "--font-zen-kaku-gothic-new",
+  subsets: ["latin", "cyrillic", "latin-ext"],
 })
